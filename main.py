a98a008ddbfc5ea0de18f446e76afc6aef7aebaa
mvdc1 <48755913+phantom-2@users.noreply.github.com>
2020-09-07 17:05:40 +1000

    first

---
+++ main.py
@@ -0,0 +1,171 @@
+import random, arcade, sys, json, os, time, http.server, socketserver, threading
+
+SPRITE_SCALING_PLAYER = 0.12
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 425
+SCREEN_TITLE = "COVID-19 Safe Game"
+endx = range(520, 600)
+endy = range(0,420)
+
+def time_format(sec):
+    mins = sec // 60
+    sec = sec % 60
+    hours = mins // 60
+    mins = mins % 60
+    if hours == 0:
+        return "{0}:{1}".format(int(mins), round(sec, 2))
+    else:
+        return "{0}:{1}:{2}".format(int(hours), int(mins), round(sec, 2))
+
+class MyGame(arcade.Window):
+
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        self.background = None
+        self.player_list = None
+
+        self.player_sprite = None
+        self.score = 0
+        self.set_mouse_visible(False)
+
+    def setup(self):
+        self.background = arcade.load_texture("assets/map.png")
+        self.player_list = arcade.SpriteList()
+        self.player_sprite = arcade.Sprite("assets/spriteSTILL.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.player_list.append(self.player_sprite)
+        print("Game has started.")
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        self.player_list.draw()
+
+    def on_mouse_motion(self, x, y, dx, dy):
+
+        # Move the center of the player sprite to match the mouse x, y
+        self.player_sprite.center_x = x
+        self.player_sprite.center_y = y
+        if x in endx and y in endy:
+            end = time.time()
+            lapse = end - start
+            print(colours.yellow + "Saving your result." + colours.reset)
+            success = False
+            try:
+                if os.path.exists("scores.json"):
+                    with open("scores.json") as scoresreader:
+                        scores = json.load(scoresreader)
+                    with open("scores.json", "w") as scoresfile:
+                        scores.append({"time": time_format(lapse), "user": os.getenv("username")})
+                        json.dump(scores, scoresfile, indent=4, sort_keys=True)
+                else:
+                    with open("scores.json", "w") as newf:
+                        json.dump([{"time": time_format(lapse), "user": os.getenv("username")}], newf, indent=4, sort_keys=True)
+                    with open("scores.json") as scoresreader:
+                        scores = json.load(scoresreader)
+                success = True
+            except:
+                print("Your score could not be saved.")
+            if success:
+                print(colours.lblue + "Your score has been saved successfully." + colours.reset)
+                if len(scores) >= 2:
+                    arcade.close_window()
+                    print("You completed the course in the following time: " + colours.yellow + time_format(lapse) + colours.reset)
+                    print("You can view how this score matches up against any previous scores by heading to " + colours.purple + "'localhost:3000'" + colours.reset + ".")
+                    for i in range(30, 0, -1):
+                        if i < 30:
+                            print(colours.clearlastline)
+                            print(colours.reset + "This program will close in " + colours.red + str(i) + colours.reset + " seconds.")
+                            sys.stdout.flush()
+                            time.sleep(1)
+                        else:
+                            print(colours.reset + "This program will close in " + colours.red + str(i) + colours.reset + " seconds.")
+                            sys.stdout.flush()
+                            time.sleep(1)
+                    sys.exit()
+                else:
+                    arcade.close_window()
+                    print("You completed the course in the following time: " + colours.yellow + time_format(lapse))
+                    print("This was your first game, you can view your score by heading to " + colours.purple + "'localhost:3000'" + colours.reset + ", but in the future you will see all of your scores which have been saved.")
+                    for i in range(30, 0, -1):
+                        if i < 30:
+                            print(colours.clearlastline)
+                            print(colours.reset + "This program will close in " + colours.red + str(i) + colours.reset + " seconds.")
+                            sys.stdout.flush()
+                            time.sleep(1)
+                        else:
+                            print(colours.reset + "This program will close in " + colours.red + str(i) + colours.reset + " seconds.")
+                            sys.stdout.flush()
+                            time.sleep(1)
+                    sys.exit()
+        else:
+            return
+
+def main():
+    print("Game rendering.")
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+def server():
+    handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", 3000), handler) as httpd:
+        httpd.serve_forever()
+
+if __name__ == "__main__":
+    try:
+        if os.name.lower() == "nt":
+            try:
+                if os.environ["COLORTERM"] == "truecolor":
+                    class colours:
+                        red = "\u001b[31m"
+                        black = "\u001b[30m"
+                        white = "\u001b[37m"
+                        lblue = "\u001b[36m"
+                        purple = "\u001b[34m"
+                        pink = "\u001b[35m"
+                        green = "\u001b[32m"
+                        yellow = "\u001b[33m"
+                        reset = "\u001b[0m"
+                        clearlastline = "\033[A                             \033[A"
+                else:
+                    class colours:
+                        red = ""
+                        black = ""
+                        white = ""
+                        lblue = ""
+                        purple = ""
+                        pink = ""
+                        green = ""
+                        yellow = ""
+                        reset = ""
+                        clearlastline = ""
+            except KeyError:
+                class colours:
+                        red = ""
+                        black = ""
+                        white = ""
+                        lblue = ""
+                        purple = ""
+                        pink = ""
+                        green = ""
+                        yellow = ""
+                        reset = ""
+                        clearlastline = ""
+            daemon = threading.Thread(name="daemon server", target=server)
+            daemon.setDaemon(True)
+            daemon.start()
+            print(colours.reset + "Server started at " + colours.green + "localhost:3000" + colours.reset + ".")
+
+            start = time.time()
+            print("Building game.")
+            main()
+
+        else:
+            print("Sorry, this program is only compatible on Windows based platforms.")
+            sys.exit()
+    except:
+        print("Sorry, this program is only compatible on Windows based platforms.")
+        sys.exit()
